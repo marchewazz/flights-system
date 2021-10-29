8c8057c7682e056acbd2b7a8ac7ffa53393596e2
marchewazz <mateuszooospot@gmail.com>
2021-10-30 00:53:04 +0200

    Order data is validated at every change to make button disabled/enabled

--- frontend/src/app/components/destination-form/destination-form.component.ts
+++ frontend/src/app/components/destination-form/destination-form.component.ts
@@ -25,7 +25,8 @@ export class DestinationFormComponent implements OnInit {
   ticketEmail = new FormControl();
   ticketCurrency : string[] = ['USD', 'JPY', 'EUR', 'PLN', 'CAD', 'CHF', 'GBP', 'AUD', 'CNY', 'RUB'];
   ticketCurrencyControl = new FormControl();
-  
+  validTicketData : boolean = false;
+
   originCityControl = new FormControl();
   destinationCityControl = new FormControl();
   constructor(private cas: CityAirportsService, private tss: TicketSystemService) {
@@ -34,6 +35,7 @@ export class DestinationFormComponent implements OnInit {
 
   ngOnInit(): void {}
   swapCities(){
+    //to do
     let origin = this.originCityControl.value;
     let destination = this.destinationCityControl.value;
     console.log(origin, destination);
@@ -119,120 +121,112 @@ export class DestinationFormComponent implements OnInit {
     //THESE VARIABLES STORE VALUES FROM FORM CONTROL, THEY ARE CODES OF THE CHOOSED AIRPORTS
     console.log(originAirports, destinationAirports);
     
-    if (originAirports !== null && destinationAirports !== null){
-      var flights : Flight[] = []
-      //LOOPING THROUGH ARRAYS OF ORIGIN AND DESTINATION AIRPORTS
-      for (var originAirport of originAirports){
-        for (var destinationAirport of destinationAirports){
-          //THESE VARIABLES ARE AIRPORTS CODE WITHOUT '-sky'
-          //IT'S BECAUSE IN DESTINATION DETAILS AND ORIGIN DETAILS CITIES ARE OFTEN IN DIFFRENT ORDER
-          let originCode = originAirport.split("-")[0]
-          let destinationCode = destinationAirport.split("-")[0]
-          this.cas.getFlights(originAirport, destinationAirport, currency).subscribe((res) => {
-            //GETTING FLIGHT INFO
-            const info : any = res
-            console.log(info);
-            if (info.Quotes.length !== 0){
-              //IF WE GOT FLIGHT OPTION IN RESPONSE WE PUSH IT TO ARRAY FLIGHTS
-              console.log(originAirport, originCode, destinationAirport, destinationCode);
-              let originCity = {
-                cityName: "",
-                airportName: "",
-                airportCode: "",
-                country: ""
-              };
-              let destinationCity = {
-                cityName: "",
-                airportName: "",
-                airportCode: "",
-                country: ""
-              };
-
-              for (var place of info.Places){
-                if (place.IataCode == originCode){
-                  originCity = {
-                    cityName: place.CityName,
-                    airportName: place.Name,
-                    airportCode: place.IataCode,
-                    country: place.CountryName
-                  }
-                }
-                if (place.IataCode == destinationCode){
-                  destinationCity = {
-                    cityName: place.CityName,
-                    airportName: place.Name,
-                    airportCode: place.IataCode,
-                    country: place.CountryName
-                  }
+    var flights : Flight[] = []
+    //LOOPING THROUGH ARRAYS OF ORIGIN AND DESTINATION AIRPORTS
+    for (var originAirport of originAirports){
+      for (var destinationAirport of destinationAirports){
+        //THESE VARIABLES ARE AIRPORTS CODE WITHOUT '-sky'
+        //IT'S BECAUSE IN DESTINATION DETAILS AND ORIGIN DETAILS CITIES ARE OFTEN IN DIFFRENT ORDER
+        let originCode = originAirport.split("-")[0]
+        let destinationCode = destinationAirport.split("-")[0]
+        this.cas.getFlights(originAirport, destinationAirport, currency).subscribe((res) => {
+          //GETTING FLIGHT INFO
+          const info : any = res
+          console.log(info);
+          if (info.Quotes.length !== 0){
+            //IF WE GOT FLIGHT OPTION IN RESPONSE WE PUSH IT TO ARRAY FLIGHTS
+            console.log(originAirport, originCode, destinationAirport, destinationCode);
+            let originCity = {
+              cityName: "",
+              airportName: "",
+              airportCode: "",
+              country: ""
+            };
+            let destinationCity = {
+              cityName: "",
+              airportName: "",
+              airportCode: "",
+              country: ""
+            };
+            for (var place of info.Places){
+              if (place.IataCode == originCode){
+                originCity = {
+                  cityName: place.CityName,
+                  airportName: place.Name,
+                  airportCode: place.IataCode,
+                  country: place.CountryName
                 }
               }
-              //SOMETIMES THERE'S ERROR WITH CITIES AND CITY DATA IS EMPTY STRING
-              //THIS FUNCTIONS SOLVES THIS PROBLEM AND JUST DOESN'T PUSH CITY LIKE IT
-              function isCityValid(city: any){
-                for(var key in city){
-                  if(city[key] == "") return false
+              if (place.IataCode == destinationCode){
+                destinationCity = {
+                  cityName: place.CityName,
+                  airportName: place.Name,
+                  airportCode: place.IataCode,
+                  country: place.CountryName
                 }
-                return true
               }
-              if(isCityValid(originCity) && isCityValid(destinationCity)){
-                flights.push({
-                carriers : info.Carriers[0].Name,
-                //API RETURNS DEPARTURE DATE AS 0:00 SO ITS CONNECTING DEPARTURE DATE WITH QUOTE TIME TO GET DIFFERENT HOUR
-                dateTime: info.Quotes[0].OutboundLeg.DepartureDate.split("T")[0]+"T"+info.Quotes[0].QuoteDateTime.split("T")[1],
-                direct: true,
-                moneyDetails : {
-                  price: info.Routes[0].Price,
-                  code: info.Currencies[0].Code,
-                  symbol: info.Currencies[0].Symbol
-                },
-                originDetails: originCity,
-                destinationDetails: destinationCity
-              })
             }
-            } 
-          })
-        }
+            //SOMETIMES THERE'S ERROR WITH CITIES AND CITY DATA IS EMPTY STRING
+            //THIS FUNCTIONS SOLVES THIS PROBLEM AND JUST DOESN'T PUSH CITY LIKE IT
+            function isCityValid(city: any){
+              for(var key in city){
+                if(city[key] == "") return false
+              }
+              return true
+            }
+            if(isCityValid(originCity) && isCityValid(destinationCity)){
+              flights.push({
+              carriers : info.Carriers[0].Name,
+              //API RETURNS DEPARTURE DATE AS 0:00 SO ITS CONNECTING DEPARTURE DATE WITH QUOTE TIME TO GET DIFFERENT HOUR
+              dateTime: info.Quotes[0].OutboundLeg.DepartureDate.split("T")[0]+"T"+info.Quotes[0].QuoteDateTime.split("T")[1],
+              direct: true,
+              moneyDetails : {
+                price: info.Routes[0].Price,
+                code: info.Currencies[0].Code,
+                symbol: info.Currencies[0].Symbol
+              },
+              originDetails: originCity,
+              destinationDetails: destinationCity
+            })
+          }
+          } 
+        })
       }
-      //AT THE END WE ARE ASSIGNING IT TO CLASS VARIABLE
-      this.flights = flights
-      console.log(this.flights);
-    } else {
-      //IF ONE OF ARRAYS IS EMPTY IT'S ERROR
-      this.dataProblem = "Missing airports";
-      console.log(this.dataProblem);
     }
+    //AT THE END WE ARE ASSIGNING IT TO CLASS VARIABLE
+    this.flights = flights
+    console.log(this.flights);
   }
-
-  orderTicket(event : any) {
-    event.preventDefault()
-    const selectedTicket = this.selectedTicketControl;
-    const nameAndSurname = this.ticketNameAndSurname.value;
+  validateTicketData(){
+    const selectedTicket = this.selectedTicketControl ? this.selectedTicketControl != '' : null;
+    const nameAndSurname = this.ticketNameAndSurname.value ? this.ticketNameAndSurname.value != '' : null;
     const email = this.ticketEmail.value
-
+    console.log(selectedTicket, nameAndSurname, email);
     function validateEmail(email: any) {
       const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(email);
     }
 
-    if (selectedTicket == undefined || nameAndSurname == null || email == null) {
-      alert('Pass data');
-    } else if(!validateEmail(email)){
-      alert('Email isn\'t valid');
-    } 
-    else {
-      const ticket = {
-        flight: selectedTicket,
-        person: {
-          email: email,
-          nameAndSurname: nameAndSurname
-        }
+    if (!validateEmail(email) || !selectedTicket || !nameAndSurname) this.validTicketData = false;
+    else this.validTicketData = true
+  }
+  orderTicket(event : any) {
+    event.preventDefault()
+    const selectedTicket = this.selectedTicketControl;
+    const nameAndSurname = this.ticketNameAndSurname.value;
+    const email = this.ticketEmail.value
+    const ticket = {
+      flight: selectedTicket,
+      person: {
+        email: email,
+        nameAndSurname: nameAndSurname
       }
-      this.tss.generateTicket(ticket).subscribe((res) =>{
-        const info : any = res
-        console.log(info);
-      })
-      alert(`Ticket is sent on email: ${email}`)
     }
+    this.tss.generateTicket(ticket).subscribe((res) =>{
+      const info : any = res
+      console.log(info);
+    })
+    alert(`Ticket is sent on email: ${email}`)
   }
 }
 
